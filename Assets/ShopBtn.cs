07237b166f82b7e9471cdf0c5a3e0bc340b7f096
Youmizu <145573094+Youmizu@users.noreply.github.com>
2025-06-17 23:42:14 +0700

    Add files via upload

---
+++ Assets/ShopBtn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShopBtn : MonoBehaviour
+{
+    public void goToShop()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+}
